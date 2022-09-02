a8fe1567239918f1008db8ad33c913e53d6bc589
BuildTools <unconfigured@null.spigotmc.org>
2022-09-02 19:31:43 +0800

    start plugin

---
+++ src/main/java/me/hifei/rewardchests/RewardChestsPlugin.java
@@ -0,0 +1,10 @@
+package me.hifei.rewardchests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RewardChestsPlugin extends JavaPlugin {
+    public static void main(String[] args) {
+        throw new RuntimeException("What are you doing? YOU ARE TRYING RUN A PLUGIN?");
+    }
+
+}
